817b0a3efd0e479c730c1e4a8d41244591bfa8a6
zhangyu <1015371461@qq.com>
2023-08-24 16:30:26 +0800

    路由的使用

--- 06_Node服务器-express/uploads/01_express基本使用.js
+++ 06_Node服务器-express/uploads/01_express基本使用.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 
 // 1. 创建express服务器
@@ -16,4 +17,24 @@ app.get('/home', (req, res) => {
 // 2. 启动服务器，并且监听端口
 app.listen(8000, () => {
     console.log("服务器启动成功")
+=======
+const express = require('express')
+
+// 1. 创建express服务器
+const app = express()
+
+// 客户端访问URL：/home和/login
+app.post('/login', (req, res) => {
+    // 处理login请求
+    res.end('hello login')
+})
+
+app.get('/home', (req, res) => {
+    res.end('hello home')
+})
+
+// 2. 启动服务器，并且监听端口
+app.listen(8000, () => {
+    console.log("服务器启动成功")
+>>>>>>> c9436da (路由的使用)
 })
