64d8a7e0c4b550b749b239f00490ca682b0123a0
zhangyu <1015371461@qq.com>
2023-08-26 13:18:02 +0800

    feat: 错误处理方案

---
+++ 07_Node服务器-koa/08_错误处理.js
@@ -0,0 +1,35 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  const isLogin = false;
+  if (!isLogin) {
+    // EventEmitter
+    ctx.app.emit('error', -1003, ctx);
+  }
+});
+
+app.on('error', (err, ctx) => {
+  const errCode = err;
+  let message = '';
+  switch (errCode) {
+    case -1001:
+      message = '账号或密码错误';
+      break;
+    case -1002:
+      message = '请求参数不正确';
+      break;
+  }
+
+  const body = {
+    err: errCode,
+    message,
+  };
+
+  ctx.body = body;
+});
+
+app.listen(8000, () => {
+  console.log('koa服务器启动成功');
+});
