d946b8e55c403d29c8be2fa80f2959cce7ec2f2a
zhangyu <1015371461@qq.com>
2023-08-26 14:34:24 +0800

    feat: express执行异步（有缺陷）

---
+++ 08_koa和express区别/04_express中间件-执行异步.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+const middleware1 = (req, res, next) => {
+  req.message = 'aaa';
+  next();
+  res.end(req.message);
+};
+
+const middleware2 = (req, res, next) => {
+  req.message += 'bbb';
+  next();
+};
+
+const middleware3 = (req, res, next) => {
+  // express中间件的执行不会等待异步操作的完成
+  //   使用 await 和async也不行，因为await next()默认返回的是viod，是不会等异步结果的
+  // 因此在第一个中间件中响应的数据仅为: aaabbb
+  axios.get('http://123.207.32.32:9001/lyric?id=167876').then((result) => {
+    req.message += result.data.lrc.lyric;
+  });
+};
+
+app.use(middleware1, middleware2, middleware3);
+
+app.listen(8000, () => {
+  console.log('服务器开启成功');
+});
