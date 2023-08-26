c13cf1f5c3118222fca09ee0daec8b7cb45177af
zhangyu <1015371461@qq.com>
2023-08-26 14:26:32 +0800

    feat: koa执行异步

---
+++ 08_koa和express区别/03_koa中间件-执行异步.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+const axios = require('axios');
+
+const app = new Koa();
+
+// 如果执行的下一个中间件是一个异步函数，那么next()默认不会等到中间件的结果，就会执行下一步操作
+const middleware1 = async (ctx, next) => {
+  ctx.request.message = 'aaa';
+  await next();
+  ctx.response.body = ctx.request.message;
+};
+
+const middleware2 = async (ctx, next) => {
+  ctx.request.message += 'bbb';
+  await next();
+};
+
+const middleware3 = async (ctx, next) => {
+  const result = await axios.get('http://123.207.32.32:9001/lyric?id=167876');
+  ctx.request.message += result.data.lrc.lyric;
+};
+
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware3);
+
+app.listen(8000, () => {
+  console.log('服务器开启成功');
+});
