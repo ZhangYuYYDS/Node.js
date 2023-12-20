068f2a1cbc53afd02694a2d7f9150a940de6ba37
zhangyu <1015371461@qq.com>
2023-12-21 00:59:35 +0800

    可写流

--- 01_Stream/02_writerStream.js
+++ 01_Stream/02_writerStream.js
@@ -1,14 +1,41 @@
 import * as fs from 'fs';
-let data = 'Hello, Node.js';
+let data = '张雨';
 
 // 创建一个可以写入的流，写入到文件 output.txt 中
-var writerStream = fs.createWriteStream('output.txt');
+var writerStream = fs.createWriteStream('output.txt', {
+    // flags: 'w', // 写入文件操作，文件已存在则清空文件内容
+    flags: 'a', // 写入文件操作，文件已存在则追加内容
+    // flags: 'wx' // 写入文件操作，若文件已存在则报错
+    // flags: 'r+' // 读取文件操作，若文件不存在则报错
+    // flags: 'rs+' // 读取文件操作，若文件不存在则报错
+    // flags: 'w+' // 读取文件操作，若文件不存在则创建
+    // flags: 'wx+' // 读取文件操作，若文件不存在则创建
+    // flags: 'a+' // 读取文件操作，若文件不存在则创建
+    // flags: 'ax+' // 读取文件操作，若文件不存在则创建
+    // flags: 'r' // 读取文件操作，若文件不存在则报错
+    // flags: 'rs' // 读取文件操作，若文件不存在则报错,
 
-// 使用 utf8 编码写入数据
-writerStream.write(data, 'utf8');
+    start: 2, // 指定写入文件的起始位置
+});
 
-// 标记文件末尾
-writerStream.end();
+// 可以监听到文件被打开
+writerStream.on('open', function (fd) {
+    console.log('文件被打开了', fd);
+});
+
+// 写入数据
+writerStream.write(data);
+
+// 标记文件末尾：当我们写入完成后，必须手动调用close/end方法
+// close : 关闭流，释放资源
+// writerStream.close();
+// end : 将最后文件写入文件中，关闭文件
+writerStream.end('hhhh');
+
+// 监听文件的关闭事件
+writerStream.on('close', function () {
+    console.log('文件被关闭');
+});
 
 // 处理流事件 --> finish、error
 writerStream.on('finish', function () {
